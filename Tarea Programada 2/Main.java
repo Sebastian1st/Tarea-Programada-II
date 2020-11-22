871655e50de56b1f8f3e48f5822f28018ef889bd
Alessandro Pani <ALESSANDRO.PANI@ucr.ac.cr>
2020-11-22 14:24:51 -0600

    prueba

--- Tarea Programada 2/Main.java
+++ Tarea Programada 2/Main.java
@@ -1,33 +1,20 @@
 
 /**
- * Write a description of class Main here.
+ * clase main 
  *
- * @author (your name)
+ * @author Alessandro Pani 
+ * @author Sebastian Soto
  * @version (a version number or a date)
  */
 public class Main
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Main
-     */
     public Main()
     {
-        // initialise instance variables
-        x = 0;
+      
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    public static void main (String args[])
     {
-        // put your code here
-        return x + y;
+       
     }
 }
